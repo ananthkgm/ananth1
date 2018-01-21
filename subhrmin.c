6755ae076ee662b8cadf21619b32670d9cd938c5
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-01-21 14:33:12 +0530

    Create subhrmin.c

---
+++ subhrmin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int h,m,hr,min,hr1,min1;
+	printf("enter time");
+	scanf("%d%d",&hr,&min);
+	printf("enter 2 time");
+	scanf("%d%d",&hr1,&min1);
+	h=hr-hr1;
+	m=min-min1;
+	printf("%d%d",h,m);
+	return 0;
+}
